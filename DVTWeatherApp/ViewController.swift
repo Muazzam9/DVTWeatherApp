70d2caf906a863cde6b7b49919bf996039fbd5bf
Muazzam <muazzam.aziz@flash.co.za>
2024-06-15 12:31:10 +0200

    Initial Commit

---
+++ DVTWeatherApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DVTWeatherApp
+//
+//  Created by Muazzam Aziz on 2024/06/15.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
